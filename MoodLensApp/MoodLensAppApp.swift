f9cb00c335888ac02cfef0307cfe6b45245fe584
Maggie <lemaggie2099@gmail.com>
2025-07-28 21:37:02 -0700

    Initial Commit

---
+++ MoodLensApp/MoodLensAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodLensAppApp.swift
+//  MoodLensApp
+//
+//  Created by Maggie Le on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MoodLensAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
